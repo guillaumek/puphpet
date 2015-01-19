20173bb2f0a591167fcd40ef46ed1a2f1f2fe7b4
guillaumek <dg.kaba@gmail.com>
2015-01-19 20:29:30 +0100

    try something else

--- archive/puphpet/shell/install-ruby.sh
+++ archive/puphpet/shell/install-ruby.sh
@@ -60,7 +60,7 @@ if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
     gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 elif [[ "${OS}" == 'centos' ]]; then
     #gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-    curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+    curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 fi
 
 curl -sSL https://get.rvm.io | bash -s stable --quiet-curl --ruby=1.9.3
